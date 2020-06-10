4d5f6b8ed4f7148918db13a3e46e4a1c30aa3aa5
Jerry <jerrylewisbh@gmail.com>
2020-06-10 16:00:45 -0300

    session 5

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void OnCollisionEnter(Collision other)
+    {
+        if ( ! other.gameObject.CompareTag("Player"))
+        {
+            Destroy(this);
+            Destroy(other.gameObject);
+        }
+        
+   
+       
+    }
+}
