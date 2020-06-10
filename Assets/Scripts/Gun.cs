2d0a864cc75e6c672f27d1599d19b07ca67a7808
Jerry <jerrylewisbh@gmail.com>
2020-06-10 16:00:45 -0300

    session 6

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+
+    public GameObject Bullet;
+    public Transform spawnPoint;
+    public float force = 200;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shot();
+        }
+        
+    }
+
+    private void Shot()
+    {
+         GameObject bullet =  Instantiate(Bullet, spawnPoint.position, Quaternion.identity);
+         Rigidbody rigidbody = bullet.GetComponent<Rigidbody>();
+         
+         rigidbody.AddForce(spawnPoint.forward * force);
+    } 
+}
---
+++ Assets/Scripts/LoadScenes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScenes : MonoBehaviour
+{
+    public void LoadTerrain()
+    {
+        SceneManager.LoadScene("Terrain");
+    }
+
+    public void LoadAR()
+    {
+        SceneManager.LoadScene("ARMobile");
+    }
+    
+}
