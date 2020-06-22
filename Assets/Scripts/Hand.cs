8a0ec7214d082fb1862d34e1ee17ca853c302b5a
Jerry <jerrylewisbh@gmail.com>
2020-06-22 15:55:28 -0300

    Session 9

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -12,10 +12,84 @@ public enum Handness
 public class Hand : MonoBehaviour
 {
     public Handness handness;
+    private Animator animator;
+    private string gripButtonAxis;
+
+    private GameObject selectedObject = null;
+
+    public void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+
+        if (handness == Handness.Right)
+        {
+            gripButtonAxis = "RightGrip";
+        }
+        else
+        {
+            gripButtonAxis = "LeftGrip";
+        }
+    }
 
     public void Update()
     {
-        float rightTriggerInput = Input.GetAxis("RightTrigger");
-        Debug.Log(rightTriggerInput);
+        
+        if(Input.GetKey(KeyCode.T))
+        {
+            transform.Translate( Vector3.forward * 20 * Time.deltaTime);
+        }
+        
+        float gripValue = Input.GetAxis(gripButtonAxis);
+        
+        if (gripValue > 0)
+        {
+            animator.SetBool("GripPressed", true);
+        }
+        else
+        {
+            animator.SetBool("GripPressed", false);
+        }
+
+        if (gripValue == 1)
+        {
+            Grab();
+        }
+
+        if (gripValue == 0)
+        {
+            Release();
+        }
+        
+    }
+
+    private void Grab()
+    {
+        if (selectedObject != null)
+        {
+            selectedObject.transform.parent = this.transform;
+            Rigidbody otherRigidbody = selectedObject.GetComponent<Rigidbody>();
+            otherRigidbody.isKinematic = false;
+        }
+    }
+
+    private void Release()
+    {
+        if (selectedObject != null)
+        {
+            selectedObject.transform.parent = null;
+            
+            Rigidbody otherRigidbody = selectedObject.GetComponent<Rigidbody>();
+            otherRigidbody.isKinematic = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        selectedObject = other.gameObject;
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        selectedObject = null;
     }
 }
