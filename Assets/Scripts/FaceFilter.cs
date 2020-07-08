7a309336dd88cb3d8a59c61554bb7051a0270f61
Jerry <jerrylewisbh@gmail.com>
2020-07-08 16:14:30 -0300

    session 14

---
+++ Assets/Scripts/FaceFilter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+
+public class FaceFilter : MonoBehaviour
+{
+
+    private ARFaceManager faceManager;
+
+    public List<Material> faceFilters;
+    private int currentIndex = 0;
+
+
+    private void Awake()
+    {
+        faceManager = GetComponent<ARFaceManager>();
+    }
+    
+    public void ChangeFilter()
+    {
+        currentIndex = currentIndex + 1;
+        if (currentIndex > faceFilters.Count - 1 )
+        {
+            currentIndex = 0;
+        }
+
+
+        foreach (ARFace face in faceManager.trackables)
+        {
+            face.GetComponent<MeshRenderer>().material = faceFilters[currentIndex];
+        }
+
+    }
+    
+}
